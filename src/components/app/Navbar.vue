c9a08f069ee7c1c1f9b0c8292f718c7a64a83c5b
admin <1111>
2022-09-05 22:56:16 +0300

    'added router-link to record'

--- src/components/app/Navbar.vue
+++ src/components/app/Navbar.vue
@@ -2,12 +2,9 @@
   <nav class="navbar orange lighten-1">
     <div class="nav-wrapper">
       <div class="navbar-left">
-
-        <a href="#" v-on:click.prevent="$emit('click')">
+        <a href="#" @click.prevent="$emit('click')">
           <i class="material-icons black-text">dehaze</i>
         </a>
-
-
         <span class="black-text">12.12.12</span>
       </div>
 
