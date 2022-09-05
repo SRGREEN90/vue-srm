2a899d008aa000fbe775acc200b58fc8a017e299
admin <1111>
2022-09-06 00:01:54 +0300

    'added changes'

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
-import router from './router'
-import store from './store'
+import router from './router/index'
+import store from './store/index'
+import dateFilter from './filters/date.filter'
+
+
+
+Vue.filter('date', dateFilter)
 
 createApp(App).use(store).use(router).mount('#app')
